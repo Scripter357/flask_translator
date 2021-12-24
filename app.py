c603de6cc967a091bec928c86fbce9b10c8f338b
Scripter357 <scripter357@gmail.com>
2021-12-24 14:01:01 +0300

    google api update

--- app.py
+++ app.py
@@ -1,41 +1,29 @@
 from flask import Flask, render_template, request
 import requests
+import sys
 
-url = "https://google-translate1.p.rapidapi.com/language/translate/v2/languages"
+url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
 
 headers = {
+    'content-type': "application/x-www-form-urlencoded",
     'accept-encoding': "application/gzip",
     'x-rapidapi-host': "google-translate1.p.rapidapi.com",
-    'x-rapidapi-key': "ef13e2c29cmsha4db27280354828p11d68fjsndf58f73edcbf"
+    'x-rapidapi-key': ""
     }
-
-response = requests.request("GET", url, headers=headers)
-
-print(response.text)
 
 
 app = Flask(__name__)
 
-my_words = {"hello": "привет", "Hello": "Привет", "no": "нет", "No": "Нет", "world": "мир", "World": "Мир"}
-
 @app.route('/', methods=['GET', 'POST'])
-def dict_page()
+def dict_page():
     if request.method == 'POST':
         word = request.form["word"]
-	if mywords[word] == None:
-           return render_template('result.html', word="This word isn't available.")
-        return render_template('result.html', word=my_words[word])
+        payload = "source=en&target=ru&format=text&q="+word
+        response = requests.request("POST", url, data=payload, headers=headers)
+        text = response.text.split(':')[3].split('}')[0]
+        return render_template('result.html', word=text)
 
     return render_template('dict.html')
 
 if __name__ == '__main__':
     app.run(port=5002)
-
-#method: GET
-#url: 'https://google-translate1.p.rapidapi.com'/language/translate/v2/languages'
-#accept-encoding: 'application/gzip'
-#x-rapidapi-host: google-translate1.p.rapidapi.com
-#x-rapidapi-key:
-
-#SQLX 30 tasks -- homeword
-#Install docker
